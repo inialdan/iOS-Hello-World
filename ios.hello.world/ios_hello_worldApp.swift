8f9b600c67261a9120520b222c1fae8723be0141
Aldan <projectarizki@gmail.com>
2021-09-25 19:25:03 +0700

    Initial Commit

---
+++ ios.hello.world/ios_hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_hello_worldApp.swift
+//  ios.hello.world
+//
+//  Created by Aldan on 25/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ios_hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
